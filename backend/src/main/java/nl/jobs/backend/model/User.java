b1223871d90cfca7aaacdc414411a8da392767dc
Yasin <>
2025-04-19 03:50:10 +0200

    Add User model

---
+++ backend/src/main/java/nl/jobs/backend/model/User.java
@@ -0,0 +1,24 @@
+package nl.jobs.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+}
