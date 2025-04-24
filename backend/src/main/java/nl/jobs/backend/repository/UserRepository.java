c2fff99fc75ade32a58592edeaf4ca5d035bbd8f
Yasin <>
2025-04-24 19:33:34 +0200

    Remove redundant findByEmail method from UserRepository interface.

--- backend/src/main/java/nl/jobs/backend/repository/UserRepository.java
+++ backend/src/main/java/nl/jobs/backend/repository/UserRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(@NotBlank String username);
-    User findByEmail(@NotBlank String email);
 }
